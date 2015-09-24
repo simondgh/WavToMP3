9fcd202c1f74c4a7863a3c5518e716f1cf62d50a
Simon <simon@simond.net>
2015-09-24 16:00:43 -0700

    New
    New

---
+++ Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace WavToMP3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxArtistName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxAlbumName = new System.Windows.Forms.TextBox();
+            this.checkBoxID3Tags = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(60, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Directory";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(60, 41);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(124, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Delete Source File(s)";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 122);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(531, 362);
+            this.textBox1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "128",
+            "320"});
+            this.comboBox1.Location = new System.Drawing.Point(337, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(286, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Bit Rate";
+            // 
+            // textBoxArtistName
+            // 
+            this.textBoxArtistName.Location = new System.Drawing.Point(144, 70);
+            this.textBoxArtistName.Name = "textBoxArtistName";
+            this.textBoxArtistName.Size = new System.Drawing.Size(187, 20);
+            this.textBoxArtistName.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Artist Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Album Name";
+            // 
+            // textBoxAlbumName
+            // 
+            this.textBoxAlbumName.Location = new System.Drawing.Point(144, 96);
+            this.textBoxAlbumName.Name = "textBoxAlbumName";
+            this.textBoxAlbumName.Size = new System.Drawing.Size(187, 20);
+            this.textBoxAlbumName.TabIndex = 7;
+            // 
+            // checkBoxID3Tags
+            // 
+            this.checkBoxID3Tags.AutoSize = true;
+            this.checkBoxID3Tags.Location = new System.Drawing.Point(289, 41);
+            this.checkBoxID3Tags.Name = "checkBoxID3Tags";
+            this.checkBoxID3Tags.Size = new System.Drawing.Size(89, 17);
+            this.checkBoxID3Tags.TabIndex = 9;
+            this.checkBoxID3Tags.Text = "Set ID3 Tags";
+            this.checkBoxID3Tags.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 510);
+            this.Controls.Add(this.checkBoxID3Tags);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxAlbumName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxArtistName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Convert WAV to MP3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxArtistName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxAlbumName;
+        private System.Windows.Forms.CheckBox checkBoxID3Tags;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+using NAudio.Lame;
+using NAudio.Wave;
+
+
+namespace WavToMP3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string directoryName;
+
+                button1.Enabled = false;
+                directoryName = folderBrowserDialog1.SelectedPath;
+                textBoxArtistName.Text = new DirectoryInfo(directoryName).Name;
+                Application.DoEvents();
+                DialogResult result = MessageBox.Show("Ready to start the conversion?", "Confirmation", MessageBoxButtons.YesNo);
+                if (result == System.Windows.Forms.DialogResult.Yes)
+                {
+                    var files = System.IO.Directory.EnumerateFiles(directoryName, "*.wav");
+
+                    foreach (string fileName in files)
+                    {
+                        textBox1.AppendText("Processing " + fileName + "\n");
+                        Application.DoEvents();
+                        WaveToMP3(fileName, fileName.Replace(".wav", ".mp3"), Int32.Parse(comboBox1.Text), textBoxArtistName.Text, textBoxAlbumName.Text, checkBoxID3Tags.Checked);
+                        if (checkBox1.Checked)
+                        {
+                            textBox1.AppendText("Deleting " + fileName + "\n");
+                            System.IO.File.Delete(fileName);
+                        }
+                    }
+
+                    textBox1.AppendText("Finished.\n");
+                }
+                button1.Enabled = true;
+            }
+        }
+
+
+
+        /// <summary>
+        ///  Convert WAV to MP3 using libmp3lame library
+        /// </summary>
+        /// <param name="waveFileName">WAV filename</param>
+        /// <param name="mp3FileName">MP3 filename</param>
+        /// <param name="bitRate">Bit rate, default 128</param>
+        /// <param name="artist">Optional artist name</param>
+        /// <param name="album">Optional album name</param>
+        /// <param name="setID3Tags">Set ID3 tags</param>
+        public static void WaveToMP3(string waveFileName, string mp3FileName, int bitRate = 128, string artist = null, string album = null, bool setID3Tags = false)
+        {
+            ID3TagData tags = new ID3TagData();
+            if (setID3Tags)
+            {
+                if (!String.IsNullOrEmpty(artist))
+                {
+                    tags.Artist = artist;
+                    tags.Album = album;
+                    tags.Genre = String.Empty;
+                }
+            }
+
+            using (var reader = new WaveFileReader(waveFileName))
+            using (var writer = new LameMP3FileWriter(mp3FileName, reader.WaveFormat, bitRate, tags))
+                reader.CopyTo(writer);
+        }
+
+        // Convert MP3 file to WAV using NAudio classes only
+        public static void MP3ToWave(string mp3FileName, string waveFileName)
+        {
+            using (var reader = new Mp3FileReader(mp3FileName))
+            using (var writer = new WaveFileWriter(waveFileName, reader.WaveFormat))
+                reader.CopyTo(writer);
+        }
+
+    }
+}
